fdf7f1706a1c0636f742ef37360a5f3f1c3fcd17
gursheen29 <gursheen2910@gmail.com>
2026-04-21 18:47:24 +0530

    Add files via upload

---
+++ session 19/prims.cpp
@@ -0,0 +1,47 @@
+class Solution {
+  public:
+    
+    int parent[1001];
+    
+    int findParent(int node)
+    {
+        if(parent[node] == node)
+            return node;
+        return parent[node] = findParent(parent[node]);
+    }
+    
+    int spanningTree(int V, vector<vector<int>>& edges) {
+        
+        for(int i = 0; i < V; i++)
+            parent[i] = i;
+        
+        sort(edges.begin(), edges.end(), [](vector<int> a, vector<int> b){
+            return a[2] < b[2];
+        });
+        
+        int totalWeight = 0;
+        int edgesUsed = 0;
+        
+        for(int i = 0; i < edges.size(); i++)
+        {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            int w = edges[i][2];
+            
+            int parentU = findParent(u);
+            int parentV = findParent(v);
+            
+            if(parentU != parentV)
+            {
+                parent[parentV] = parentU;
+                totalWeight += w;
+                edgesUsed++;
+                
+                if(edgesUsed == V - 1)
+                    break;
+            }
+        }
+        
+        return totalWeight;
+    }
+};
